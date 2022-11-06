dc3e0cabd8029e6a4c02bb15a6b0b88dffd10f33
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-11-06 16:22:41 +0100

    Домашнє завдання №4 React

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css';
 import { toast } from 'react-toastify';
 
-export default class Searchbar extends Component {
-  state = {
-    query: '',
+export default function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = e => {
+    setQuery(e.target.value.toLowerCase());
   };
 
-  handleInputChange = e => {
-    this.setState({ query: e.target.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.query.trim() === '') {
-      toast.error(
-        'Введіть будь ласка пошуковий запит, щоб ми знайшли Вам картинки'
-      );
+    if (query.trim() === '') {
+      toast.error('Enter your search query');
       return;
     }
 
-    this.props.onSubmit(this.state.query);
+    onSubmit(query);
   };
 
-  render() {
-    return (
-      <header className={styles.searchbar}>
-        <form className={styles.searchForm} onSubmit={this.handleSubmit}>
-          <input
-            onInput={this.handleInputChange}
-            className={styles.searchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
+  return (
+    <header className={styles.searchbar}>
+      <form className={styles.searchForm} onSubmit={handleSubmit}>
+        <input
+          onInput={handleInputChange}
+          className={styles.searchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+        />
+        <button type="submit" className={styles.searchFormButton}>
+          <img
+            className={styles.icon}
+            src="https://png.pngtree.com/png-vector/20191018/ourlarge/pngtree-search-vector-icon-png-image_1824209.jpg"
+            width="40"
+            alt="icon"
           />
-          <button className={styles.searchFormButton} type="submit">
-            <img
-              className={styles.icon}
-              src="https://png.pngtree.com/png-vector/20191018/ourlarge/pngtree-search-vector-icon-png-image_1824209.jpg"
-              width="40"
-              alt="icon"
-            />
-          </button>
-        </form>
-      </header>
-    );
-  }
+        </button>
+      </form>
+    </header>
+  );
 }
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
